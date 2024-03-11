16bc86c8e63e41a7e45a43cd44ce880c71c743f3
Takuya338 <118358594+Takuya338@users.noreply.github.com>
2024-03-11 20:41:06 +0900

    AWSロジックのひな型が完成

---
+++ app/AWS/Logic.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ *
+ * Logic.php
+ * 2024/03/10 後藤卓也
+ * AWSのロジッククラス  
+ *
+ */
+
+namespace App\AWS;
+
+use App\Enums\ResultCode;  // 処理結果の定数をインポート
+
+class Logic
+{
+    // プロパティ、メソッドをここに追加します
+
+    public function __construct()
+    {
+        // コンストラクタの内容をここに書きます
+    }
+
+    public function login($email, $password)
+    {
+        // ログイン処理をここに書きます
+    }
+
+    public function logout()
+    {
+        // ログアウト処理をここに書きます
+    }
+}
