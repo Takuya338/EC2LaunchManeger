8284d0ab8c7cc387d727da70d734e7fea5e17457
Takuya338 <118358594+Takuya338@users.noreply.github.com>
2024-03-11 20:46:03 +0900

    viewの修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,11 @@
                     @csrf
                     <div class="form-group">
                         <label for="emailInput">メールアドレス</label>
-                        <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="メールアドレスを入力">
+                        <input type="email" class="form-control" id="emailInput" name="email" aria-describedby="emailHelp" placeholder="メールアドレスを入力">
                     </div>
                     <div class="form-group">
                         <label for="passwordInput">パスワード</label>
-                        <input type="password" class="form-control" id="passwordInput" placeholder="パスワードを入力">
+                        <input type="password" class="form-control" id="passwordInput" name="password" placeholder="パスワードを入力">
                     </div>
                     @if (session('message'))
                     <p class="text-danger">{{ session('message') }}</p>
